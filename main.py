449b4dd8deb94722faf54370c8a6b0a3b5000e04
liamhorne98 <71072743+liamhorne98@users.noreply.github.com>
2020-11-13 13:49:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def addition(num1,num2):
+    answer=num1+num2
+    return answer
+first_number=int(input("please enter first number"))
+second_number=int(input("please enter second number"))
+result=addition(first_number,second_number)
+print(result)
